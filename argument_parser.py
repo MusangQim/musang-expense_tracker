e8566eaf16b457c4ad2a87c4f2fdf155f2aaf961
MusangQim <musangqim03@gmail.com>
2026-06-11 12:10:36 +0800

    Add: argument_parse using argparse

---
+++ argument_parser.py
@@ -0,0 +1,16 @@
+import argparse
+
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("nama")
+args = parser.parse_args()
+print(f"Hello, {args.nama}!")
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("nama")
+parser.add_argument("--umur")
+args = parser.parse_args()
+print(F"Hello, {args.nama}")
+if args.umur:
+    print(f"Umur anda {args.umur} tahun.")
