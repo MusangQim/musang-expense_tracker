81c572eb8b936dad6905fb359290b8102687d5f1
MusangQim <musangqim03@gmail.com>
2026-06-13 23:28:20 +0800

    learning about argparse

--- argument_parser.py
+++ argument_parser.py
@@ -1,5 +1,7 @@
 import argparse
 
+
+# Learning Process about argparse
 '''
 parser = argparse.ArgumentParser()
 parser.add_argument("nama")
@@ -7,6 +9,7 @@ args = parser.parse_args()
 print(f"Hello, {args.nama}!")
 '''
 
+'''
 parser = argparse.ArgumentParser()
 parser.add_argument("nama")
 parser.add_argument("--umur")
@@ -14,3 +17,4 @@ args = parser.parse_args()
 print(F"Hello, {args.nama}")
 if args.umur:
     print(f"Umur anda {args.umur} tahun.")
+'''
